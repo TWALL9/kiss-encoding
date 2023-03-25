81f10499e503afb03ea2631d22117210c56cf3ec
Tom Wallis <thomaswallis0@gmail.com>
2023-03-24 22:44:20 -0400

    Add alloc feature

--- src/encode.rs
+++ src/encode.rs
@@ -1,4 +1,9 @@
-use core::fmt::Write;
+use core::{fmt::Write, result::Result};
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+#[cfg(feature = "alloc")]
+use alloc::{vec, vec::Vec};
 
 use crate::constants::{FEND, FESC, TFEND, TFESC};
 use crate::error::EncodeError;
@@ -30,5 +35,27 @@ pub fn encode_write<W: Write>(w: &mut W, port: u8, msg: &[u8]) -> Result<(), Enc
     w.write_char(FEND as char)
         .map_err(|_| EncodeError::WriteError)?;
 
-    Ok(())
+    Result::Ok(())
+}
+
+#[cfg(feature = "alloc")]
+pub fn encode(port: u8, msg: &[u8]) -> Result<Vec<u8>, EncodeError> {
+    let port_nib = (port & 0x0F) << 4;
+    let mut v = vec![FEND, port_nib];
+
+    for b in msg.iter() {
+        if *b == FEND {
+            v.push(FESC);
+            v.push(TFEND);
+        } else if *b == FESC {
+            v.push(FESC);
+            v.push(TFESC);
+        } else {
+            v.push(*b);
+        }
+    }
+
+    v.push(FEND);
+
+    Result::Ok(v)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,13 @@ pub mod error;
 mod test {
     use super::*;
 
-    use decode::{DataFrame, DecodedVal};
     use constants::{FEND, FESC, TFEND, TFESC};
+    use decode::{DataFrame, DecodedVal};
     use error::DecodeError;
 
     #[test]
     fn decode_basics() {
-        let test: [u8; 7]= [0xC0, 0x00, 0x54, 0x45, 0x53, 0x54, 0xC0];
+        let test: [u8; 7] = [0xC0, 0x00, 0x54, 0x45, 0x53, 0x54, 0xC0];
 
         let mut decoder = DataFrame::default();
         let start_fend = decoder.decode_byte(test[0]).unwrap().unwrap();
@@ -40,7 +40,7 @@ mod test {
 
         let _start = decoder.decode_byte(FEND).unwrap().unwrap();
         let _port = decoder.decode_byte(0).unwrap().unwrap();
-        
+
         for t in test.iter() {
             assert!(decoder.decode_byte(*t).is_ok())
         }
@@ -58,7 +58,10 @@ mod test {
         assert_eq!(fesc, Some(DecodedVal::Escape));
 
         let bad_escape_val = decoder.decode_byte(bogus[3]);
-        assert!(matches!(bad_escape_val, Err(DecodeError::IncorrectValAfterEscape(0))));
+        assert!(matches!(
+            bad_escape_val,
+            Err(DecodeError::IncorrectValAfterEscape(0))
+        ));
 
         let ignored = decoder.decode_byte(bogus[4]).unwrap();
         assert!(ignored.is_none());
